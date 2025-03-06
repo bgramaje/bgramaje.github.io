f0d74b69587bea5ede7a93f882cacdf610103c54
bgramaje <boraal.dev@gmail.com>
2025-03-06 22:55:25 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,7 @@ export const viewport: Viewport = {
 
 export const metadata: Metadata = {
   title: 'boraal | Borja Albert Gramaje',
-  description:
-    'Nim is a free and open-source personal website template built with Next.js 15, React 19 and Motion-Primitives.',
+  description: 'Personal webpage and portfolio from Borja Albert Gramaje',
 }
 
 const geist = Geist({
